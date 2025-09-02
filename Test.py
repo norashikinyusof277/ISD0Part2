4cc89b8f1da04483f8b88e22160f0014cd496bcc
norashikinyusof277 <norashikin.yusof277@gmail.com>
2025-09-02 10:30:46 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,11 +1,1 @@
-import streamlit as st
-
-# Title
-st.title("Hello Streamlit 👋")
-
-# Input
-name = st.text_input("Enter your name:")
-
-# Button
-if st.button("Say Hello"):
-    st.write(f"Hello, {name}!")
+streamlit run app.py
